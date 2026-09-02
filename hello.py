6a7cc2652c980c523a51225fcb34d94bd3049cb1
nortondev <norton.rodrigues@aluno.ifsp.edu.br>
2026-09-02 17:49:02 +0000

    BD visível

--- hello.py
+++ hello.py
@@ -68,7 +68,10 @@ def index():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.name.data)
+
+            role_usuario = Role.query.filter_by(name="User").first()
+
+            user = User(username=form.name.data, role=role_usuario)
             db.session.add(user)
             db.session.commit()
             session['known'] = False
@@ -76,5 +79,7 @@ def index():
             session['known'] = True
         session['name'] = form.name.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
+
+    pessoas = User.query.all()
+    return render_template('index.html', form=form, pessoas=pessoas, name=session.get('name'),
                            known=session.get('known', False))
